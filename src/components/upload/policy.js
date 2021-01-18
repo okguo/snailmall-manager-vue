24f1afb0109fcfd71c68ec243ec497b1f4475070
Guoyongfu <Guoyongfu.1@hotmail.com>
2021-01-18 09:18:54 +0800

    文件上传接入

--- src/components/upload/policy.js
+++ src/components/upload/policy.js
@@ -2,7 +2,7 @@ import http from '@/utils/httpRequest.js'
 export function policy() {
    return  new Promise((resolve,reject)=>{
         http({
-            url: http.adornUrl("/thirdparty/oss/policy"),
+            url: http.adornUrl("/thirdParty/oss/policy"),
             method: "get",
             params: http.adornParams({})
         }).then(({ data }) => {
